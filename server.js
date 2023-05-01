3c1776d061aa3b44bba660133570a7955961e004
Bora <bora@Bora-MacBook-Air.local>
2023-05-01 15:12:20 +0300

    add client url

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 
-const { connectToDB } = require("./config/db");
+const { connectToDB } = require('./config/db');
 
-const userRoutes = require("./routes/userRoutes");
-const chatRoutes = require("./routes/chatRoutes");
-const messageRoutes = require("./routes/messageRoutes");
+const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 
-const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 connectToDB().then(() => {
-  console.log("Connected to MongoDB");
+  console.log('Connected to MongoDB');
 });
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/user", userRoutes);
-app.use("/api/chat", chatRoutes);
-app.use("/api/message", messageRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
+app.use('/api/message', messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
@@ -30,39 +30,39 @@ const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-const io = require("socket.io")(server, {
+const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
   },
 });
 
-io.on("connection", (socket) => {
-  socket.on("setup", (userData) => {
+io.on('connection', (socket) => {
+  socket.on('setup', (userData) => {
     socket.join(userData._id);
-    socket.emit("connected");
+    socket.emit('connected');
   });
 
-  socket.on("join chat", (room) => {
+  socket.on('join chat', (room) => {
     socket.join(room);
   });
 
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  socket.on('typing', (room) => socket.in(room).emit('typing'));
+  socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
 
-  socket.on("new message", (newMessageReceived) => {
+  socket.on('new message', (newMessageReceived) => {
     let chat = newMessageReceived.chat;
 
-    if (!chat.users) return console.log("Chat.users is not available");
+    if (!chat.users) return console.log('Chat.users is not available');
 
     chat.users.forEach((user) => {
       if (user._id === newMessageReceived.sender._id) return;
 
-      socket.to(user._id).emit("message received", newMessageReceived);
+      socket.to(user._id).emit('message received', newMessageReceived);
     });
   });
 
-  socket.off("setup", () => {
+  socket.off('setup', () => {
     socket.leave(userData._id);
   });
 });
